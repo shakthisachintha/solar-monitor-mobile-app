f28f867a4456f7fe788821e5b00546f8c8328726
Shakthi Sachintha <43114582+shakthisachintha@users.noreply.github.com>
2022-11-10 19:24:14 +0530

    added new types

---
+++ src/types/index.tsx
@@ -0,0 +1,25 @@
+export enum Units {
+    POWER = 1,
+    CURRRENT = 2,
+    VOLTAGE = 3,
+    PERCENTAGE = 4,
+    TEMPERATURE = 5,
+    FREQUENCY = 6
+}
+
+export enum OutputPriority {
+    SOL = 0,
+    UTI = 1,
+    SBU = 2,
+}
+
+export enum ChargerPriority {
+    CSO = 0,
+    CUB = 1,
+    SNU = 2,
+    OSO = 3,
+}
+
+
+
+
