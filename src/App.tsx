ef92d5189c69a03add6e137ee2d2a0ad6891b8b5
Shakthi Sachintha <43114582+shakthisachintha@users.noreply.github.com>
2022-11-10 19:24:30 +0530

    removed unwanted packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
-  SafeAreaView, StyleSheet, Dimensions, View, Text, ScrollView
+  SafeAreaView, StyleSheet, ScrollView
 } from 'react-native';
-import Icon from "react-native-vector-icons/FontAwesome";
 import { Provider } from 'react-redux';
+import EventSource from "react-native-sse";
 import AppHeader from './components/AppHeader/AppHeader';
 import { BatteryWidget, InverterWidget, SolarWidget, UtilityWidget } from './components/Widgets';
 import { LocaleContext } from './i18n/LocaleContext';
 import LocaleService from './i18n/LocaleService';
 import { store } from './redux/';
-import WelcomeScreen from './screens/welcome/WelcomeScreen';
 
 const language = 'eng';
 const messageService = LocaleService(language);
 
 const App = () => {
+  useEffect(() => {
+    const es = new EventSource("https://3c71-175-157-164-182.in.ngrok.io/stream");
+
+    es.addEventListener("open", (event) => {
+      console.log("Open SSE connection.");
+    });
+
+    es.addEventListener("message", (event) => {
+      console.log("New message event:", event.data);
+    });
+
+    es.addEventListener("error", (event) => {
+      if (event.type === "error") {
+        console.error("Connection error:", event.message);
+      } else if (event.type === "exception") {
+        console.error("Error:", event.message, event.error);
+      }
+    });
+
+    es.addEventListener("close", (event) => {
+      console.log("Close SSE connection.");
+    });
+  }, []);
+
   return (
     <Provider store={store}>
       <LocaleContext.Provider value={messageService}>
